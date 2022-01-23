a802d08fec9c8c2471cb1047c1572060e5747f23
Maxfergie <85373655+Maxfergie@users.noreply.github.com>
2022-01-23 14:14:34 -0500

    Add files via upload

---
+++ Codecademy_Life_Expectency_Deep_Learning/Codecademy_Life_Expectancy.py
@@ -0,0 +1,68 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import InputLayer
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import Adam
+
+dataset = pd.read_csv('life_expectancy.csv')
+#print(dataset.describe())
+
+dataset = dataset.drop(['Country'], axis=1)
+labels = dataset.iloc[:,-1]
+
+#PREPROCESSING
+#one-hot-encoding for categorical columns
+features = pd.get_dummies(dataset)
+#split data into training and testing batches
+features_train, features_test, labels_train, labels_test =  train_test_split(features,
+                                labels,
+                                test_size=.33,
+                                random_state=42)
+#define numerical data
+numerical_feats = features.select_dtypes(include=['float64', 'int64'])
+numerical_columns = numerical_feats.columns
+#standardize data
+ct = ColumnTransformer([("only numeric",
+                          StandardScaler(),
+                          numerical_columns)],
+                          remainder='passthrough')
+features_train_scaled = ct.fit_transform(features_train)
+features_test_scaled = ct.fit_transform(features_test)
+
+#MODEL
+#define model used
+my_model = Sequential()
+#establish and load input layer
+input = InputLayer(input_shape=(features.shape[1],))
+my_model.add(input)
+#add hidden layers and activation function
+my_model.add(Dense(64, activation="relu"))
+#add output layer
+my_model.add(Dense(1))
+print(my_model.summary())
+
+#OPTIMIZE AND COMPILE
+opt = Adam(learning_rate=0.01)
+my_model.compile(loss='mse',
+                 metrics='mae',
+                 optimizer=opt)
+
+#FITTING AND EVALUATION
+#fit model
+my_model.fit(features_train_scaled,
+             labels_train,
+             epochs=35,
+             batch_size=1,
+             verbose=1)
+#evaluate loss
+res_mse, res_mae = my_model.evaluate(features_train_scaled,
+                                     labels_train,
+                                     verbose=0)
+
+
+print("--------------------------------")
+print("Final Loss calculates to:", res_mse)
+print("Final Mean Absolute Error calculates to:", res_mae)
