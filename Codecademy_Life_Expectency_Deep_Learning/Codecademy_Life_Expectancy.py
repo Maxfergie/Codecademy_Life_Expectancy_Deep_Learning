0ce83a3e1e8a6b15dd36199c931a1ffa019c084a
Maxwell T. Ferguson <85373655+Maxfergie@users.noreply.github.com>
2022-01-23 15:25:11 -0500

    Update Codecademy_Life_Expectancy.py

--- Codecademy_Life_Expectency_Deep_Learning/Codecademy_Life_Expectancy.py
+++ Codecademy_Life_Expectency_Deep_Learning/Codecademy_Life_Expectancy.py
@@ -1,11 +1,15 @@
 import pandas as pd
+
 from sklearn.model_selection import train_test_split
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import StandardScaler
+
 from tensorflow.keras import Sequential
+from tensorflow.keras import layers
 from tensorflow.keras.layers import InputLayer
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import EarlyStopping
 
 dataset = pd.read_csv('life_expectancy.csv')
 #print(dataset.describe())
@@ -18,9 +22,9 @@ labels = dataset.iloc[:,-1]
 features = pd.get_dummies(dataset)
 #split data into training and testing batches
 features_train, features_test, labels_train, labels_test =  train_test_split(features,
-                                                                             labels,
-                                                                             test_size=.33,
-                                                                             random_state=42)
+                                labels,
+                                test_size=.33,
+                                random_state=42)
 #define numerical data
 numerical_feats = features.select_dtypes(include=['float64', 'int64'])
 numerical_columns = numerical_feats.columns
@@ -30,7 +34,7 @@ ct = ColumnTransformer([("only numeric",
                           numerical_columns)],
                           remainder='passthrough')
 features_train_scaled = ct.fit_transform(features_train)
-features_test_scaled = ct.fit_transform(features_test)
+features_test_scaled = ct.transform(features_test)
 
 #MODEL
 #define model used
@@ -39,10 +43,12 @@ my_model = Sequential()
 input = InputLayer(input_shape=(features.shape[1],))
 my_model.add(input)
 #add hidden layers and activation function
-my_model.add(Dense(64, activation="relu"))
+my_model.add(layers.Dense(128, activation='relu'))
+my_model.add(layers.Dropout(0.2))
+my_model.add(layers.Dense(64, activation='relu'))
 #add output layer
 my_model.add(Dense(1))
-print(my_model.summary())
+#print(my_model.summary())
 
 #OPTIMIZE AND COMPILE
 opt = Adam(learning_rate=0.01)
@@ -51,12 +57,18 @@ my_model.compile(loss='mse',
                  optimizer=opt)
 
 #FITTING AND EVALUATION
+#set up early stopping
+stop = EarlyStopping(monitor='loss',
+                     mode='min',
+                     verbose=1,
+                     patience=40)
 #fit model
 my_model.fit(features_train_scaled,
              labels_train,
              epochs=35,
              batch_size=1,
-             verbose=1)
+             verbose=1,
+             callbacks=[stop])
 #evaluate loss
 res_mse, res_mae = my_model.evaluate(features_train_scaled,
                                      labels_train,
